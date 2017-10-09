2726612f853790fa9794c3ae64571ab08640f383
sur5ive <karpo.korotko@gmail.com>
2017-10-10 00:12:45 +0300

    post test2

--- server/config.js
+++ server/config.js
@@ -2,9 +2,11 @@ Router.route( "/football-data.events", { where: "server" } )
 .get( function() {
   // If a GET request is made, return the user's profile.
   console.log("Football data get was submitted");
+  this.response.statusCode = 200;
 })
 .post( function() {
     console.log("Football data post was submitted");
+    this.response.statusCode = 200;
   // If a POST request is made, create the user's profile.
 })
 .put( function() {
